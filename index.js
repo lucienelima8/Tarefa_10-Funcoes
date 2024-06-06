6d592b2f48bcd3b7d8e1be3c05231c727bd1727c
Luciene <lucienedelima8@gmail.com>
2024-06-06 19:45:44 -0300

    fazendo o js

---
+++ index.js
@@ -0,0 +1,13 @@
+function calcular () {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const num3 = parseFloat(document.getElementById('num3').value);
+    const num4 = parseFloat(document.getElementById('num4').value);
+    const num5 = parseFloat(document.getElementById('num5').value);
+
+    const media = (num1 + num2 + num3 + num4 + num5) / 5;
+
+    document.getElementById('resultArea').innerHTML = `Sua média aritmética é: ` + media;
+
+}
+
